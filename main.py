d1f2663dbdbeae1f525bf2b069c7faf752b3e1f3
Banking simulations <ladimp11@gmail.com>
2026-07-10 00:10:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+from bank import Bank
 
+
+bank = Bank()
+
+
+kamil = bank.create_account(
+    "001",
+    "Kamil",
+    1000
+)
+
+
+ivan = bank.create_account(
+    "002",
+    "Ivan",
+    500
+)
+
+
+kamil.deposit(500)
+
+kamil.transfer(
+    ivan,
+    300
+)
+
+
+print(
+    "Kamil:",
+    kamil.get_balance()
+)
+
+
+print(
+    "Ivan:",
+    ivan.get_balance()
+)
+
+
+print("\nHistory:")
+kamil.show_history()
