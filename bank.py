582424485dee9f85fc9813df62c4e0fcb8ac3f88
Banking simulations <ladimp11@gmail.com>
2026-07-10 00:08:09 +0300

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,1 +1,25 @@
+from account import BankAccount
 
+
+class Bank:
+
+    def __init__(self):
+        self.accounts = {}
+
+
+    def create_account(self, number, owner, balance=0):
+
+        account = BankAccount(
+            number,
+            owner,
+            balance
+        )
+
+        self.accounts[number] = account
+
+        return account
+
+
+    def find_account(self, number):
+
+        return self.accounts.get(number)
